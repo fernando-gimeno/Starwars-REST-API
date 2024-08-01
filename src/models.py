38eb50958f0431c72b56c35102b1513a625c622a
Fernando Gimeno <fernandogimeno.ief@gmail.com>
2024-08-01 12:44:27 +0000

    New properties added to Models

--- src/models.py
+++ src/models.py
@@ -34,6 +34,7 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            "is_active": self.is_active,
             "favorites": self.favorites.serialize() if self.favorites else list()
         }
     
@@ -60,7 +61,12 @@ class People(db.Model):
     name = db.Column(db.String(200), nullable=False, unique=True)
     gender = db.Column(db.String(200), nullable=False)
     height = db.Column(db.Float, nullable=True)
-    weight = db.Column(db.Float, nullable=True)
+    mass = db.Column(db.Float, nullable=True)
+    hair_color = db.Column(db.String(200), nullable=True)
+    skin_color = db.Column(db.String(200), nullable=True)
+    eye_color = db.Column(db.String(200), nullable=True)
+    birth_year = db.Column(db.String(200), nullable=True)
+    
     
     def __repr__(self):
         return '<People %r>' % self.name
@@ -71,7 +77,11 @@ class People(db.Model):
             "name": self.name,
             "gender": self.gender,
             "height": self.height,
-            "weight": self.weight
+            "mass": self.mass,
+            "hair_color": self.hair_color,
+            "skin_color": self.skin_color,
+            "eye_color": self.eye_color,
+            "birth_year": self.birth_year
         }
         
 class Planet(db.Model):
@@ -80,8 +90,11 @@ class Planet(db.Model):
     terrain = db.Column(db.String(200), nullable=True)
     climate = db.Column(db.String(200), nullable=True)
     population = db.Column(db.Integer, nullable=True)
-    gravity = db.Column(db.Float, nullable=True)
+    gravity = db.Column(db.String(200), nullable=True)
     diameter = db.Column(db.Integer, nullable=True)
+    rotation_period = db.Column(db.Integer, nullable=True)
+    orbital_period = db.Column(db.Integer, nullable=True)
+    surface_water = db.Column(db.Integer, nullable=True)
     
     def __repr__(self):
         return '<Planet %r' % self.name
@@ -94,16 +107,27 @@ class Planet(db.Model):
             "climate": self.climate,
             "population": self.population,
             "gravity": self.gravity,
-            "diameter": self.diameter
+            "diameter": self.diameter,
+            "rotation_period": self.rotation_period,
+            "orbital_period": self.orbital_period,
+            "surface_water": self.surface_water
         }
 
 class Vehicle(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(200), nullable=False)
-    model = db.Column(db.String(200), nullable=False)
-    manufacturer = db.Column(db.String(200), nullable=False)
-    cost = db.Column(db.Float, nullable=False)
-    capacity = db.Column(db.Float, nullable=False)
+    name = db.Column(db.String(200), nullable=False, unique=True)
+    model = db.Column(db.String(200), nullable=True)
+    vehicle_class = db.Column(db.String(200), nullable=True)
+    manufacturer = db.Column(db.String(200), nullable=True)
+    cost_in_credits = db.Column(db.String(200), nullable=True)
+    length = db.Column(db.String(50), nullable=True)
+    crew = db.Column(db.String(50), nullable=True)
+    passengers = db.Column(db.String(50), nullable=True)
+    max_atmosphering_speed = db.Column(db.String(50), nullable=True)
+    cargo_capacity = db.Column(db.String(50), nullable=True)
+    consumables = db.Column(db.String(50), nullable=True)
+    capacity = db.Column(db.Float, nullable=True)
+    
     
     def __repr__(self):
         return '<Vehicle %r>' % self.name
@@ -113,7 +137,14 @@ class Vehicle(db.Model):
             "id": self.id,
             "name": self.name,
             "model": self.model,
+            "vehicle_class": self.vehicle_class,
             "manufacturer": self.manufacturer,
-            "cost": self.cost,
+            "cost_in_credits": self.cost_in_credits,
+            "length": self.length,
+            "crew": self.crew,
+            "passengers": self.passengers,
+            "max_atmosphering_speed": self.max_atmosphering_speed,
+            "cargo_capacity": self.cargo_capacity,
+            "consumables": self.consumables,
             "capacity": self.capacity
         }
